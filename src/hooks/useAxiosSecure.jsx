2cc5366fbdea9871b944d876f633e7b83cfc3a99
TaiebaTasnim <tasnimtaieba111@gmail.com>
2025-01-26 19:32:07 +0600

    url setting done

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://fit-me-server-site.vercel.app'
 })
 
 const useAxiosSecure = () => {
